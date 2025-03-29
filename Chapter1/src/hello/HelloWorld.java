72a1a857b3f299d3e9f0e28f947aac8638efcbd5
raver0724 <raver0724@gmail.com>
2025-03-29 14:18:38 +0900

    반복문

---
+++ Chapter1/src/hello/HelloWorld.java
@@ -0,0 +1,19 @@
+package hello;
+
+/*
+ * 파일명 - hellojava.java
+ *  만든이 - 권우진
+ *  프로그램 - hello world를 출력하는 프로그램
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// system - 클래스, out - 패키지, print() - 함수(메서드)]
+		// println() - 줄바꿈이 되는 함수
+		System.out.println("Hello~ World");
+		System.out.println("안녕~ 세계야!");
+
+	}
+
+}
